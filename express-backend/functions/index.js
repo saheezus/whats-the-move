5642224e69ac5a64188c3ae14b2c0b8b3fefcfef
faris <farisshaik1@gmail.com>
2024-11-16 17:52:58 -0600

    backend_works?

--- express-backend/functions/index.js
+++ express-backend/functions/index.js
@@ -1,17 +1,15 @@
-const functions = require('firebase-functions');
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
+const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
-// Initialize Express app
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Sample endpoint
-app.get('/', (req, res) => {
-    res.json({ message: 'Hello from Firebase Functions!' });
+// Sample route
+app.get("/greeting", (req, res) => {
+    res.json({message: "Hello from Firebase Functions!"});
 });
 
-// Export the app as a Firebase Function
-exports.api = functions.region('us-central1').https.onRequest(app);
+exports.api = functions.region("us-central1").https.onRequest(app);
